82ed3b93c20074f3eb7649f0b6f06cd4adb42233
HEnquist <henrik.enquist@gmail.com>
2020-08-05 21:55:53 +0200

    Fix exit command

--- src/bin.rs
+++ src/bin.rs
@@ -430,6 +430,10 @@ fn main() {
         debug!("Wait for config");
         while new_config.lock().unwrap().is_none() {
             trace!("waiting...");
+            if signal_exit.load(Ordering::Relaxed) == 1 {
+                // exit requested 
+                break;
+            }
             thread::sleep(delay);
         }
         debug!("Config ready");
@@ -450,7 +454,8 @@ fn main() {
             }
             Ok(ExitState::Exit) => {
                 debug!("Exiting");
-                if !wait {
+                if !wait || signal_exit.load(Ordering::Relaxed) == 1 {
+                    // wait mode not active, or exit requested 
                     break;
                 }
             }
