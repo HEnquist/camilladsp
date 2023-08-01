ed05ee6c24754ad6b6588c2096857509db1ed559
Henrik <henrik.enquist@gmail.com>
2023-08-01 20:24:21 +0200

    Convert test to pytest

--- testscripts/config_load_test/test_set_config.py
+++ testscripts/config_load_test/test_set_config.py
@@ -1,64 +1,57 @@
 import time
 import camilladsp
+import pytest
 
-cdsp = camilladsp.CamillaClient("localhost", 1234)
-cdsp.connect()
 
-# Read the configs
-configs = []
+
+@pytest.fixture
+def camillaclient():
+    cdsp = camilladsp.CamillaClient("localhost", 1234)
+    cdsp.connect()
+    yield cdsp
+
+# Read the CONFIGS
+CONFIGS = []
 for n in range(4):
     with open(f"conf{n+1}.yml") as f:
-        configs.append(f.read())
+        CONFIGS.append(f.read())
 
-# Apply them all slowly
-print("Changing slowly")
-for n in range(4):
-    print(f"Set conf{n+1}")
-    cdsp.config.set_active_raw(configs[n])
-    time.sleep(2)
-    conf = cdsp.config.active()
-    print("active:", conf["filters"]["testfilter"]["description"])
-    assert conf["filters"]["testfilter"]["description"] == f"nbr {n+1}"
+def test_slow_via_ws(camillaclient):
+    # Apply them all slowly
+    print("Changing slowly")
+    for n in range(4):
+        print(f"Set conf{n+1}")
+        camillaclient.config.set_active_raw(CONFIGS[n])
+        time.sleep(2)
+        conf = camillaclient.config.active()
+        print("active:", conf["filters"]["testfilter"]["description"])
+        assert conf["filters"]["testfilter"]["description"] == f"nbr {n+1}"
 
 
 # Apply them with short delay
-print("Changing with short delay")
-print("Set conf1")
-cdsp.config.set_active_raw(configs[0])
-time.sleep(1)
-conf = cdsp.config.active()
-print("active:", conf["filters"]["testfilter"]["description"])
-assert conf["filters"]["testfilter"]["description"] == "nbr 1"
-print("Set conf2, 3, 4, 2, 3, 4, ...")
-for _ in range(100):
-    print(".", end="", flush=True)
-    cdsp.config.set_active_raw(configs[1])
-    time.sleep(0.1)
-    cdsp.config.set_active_raw(configs[2])
-    time.sleep(0.1)
-    cdsp.config.set_active_raw(configs[3])
-    time.sleep(0.5)
-    conf = cdsp.config.active()
-    desc = conf["filters"]["testfilter"]["description"]
-    assert conf["filters"]["testfilter"]["description"] == "nbr 4", f"{desc} != nbr 4"
+def change_with_delay(camillaclient, delay, reps):
+    print(f"Changing with {1000*delay} ms delay")
+    print("Set conf1")
+    camillaclient.config.set_active_raw(CONFIGS[0])
+    time.sleep(1)
+    conf = camillaclient.config.active()
+    print("active:", conf["filters"]["testfilter"]["description"])
+    assert conf["filters"]["testfilter"]["description"] == "nbr 1"
+    print("Set conf2, 3, 4, 2, 3, 4, ...")
+    for _ in range(reps):
+        print(".", end="", flush=True)
+        camillaclient.config.set_active_raw(CONFIGS[1])
+        time.sleep(delay)
+        camillaclient.config.set_active_raw(CONFIGS[2])
+        time.sleep(delay)
+        camillaclient.config.set_active_raw(CONFIGS[3])
+        time.sleep(0.5)
+        conf = camillaclient.config.active()
+        desc = conf["filters"]["testfilter"]["description"]
+        assert conf["filters"]["testfilter"]["description"] == "nbr 4", f"{desc} != nbr 4"
 
-# Apply them with very short delay
-print("Changing fast")
-print("Set conf1")
-cdsp.config.set_active_raw(configs[0])
-time.sleep(1)
-conf = cdsp.config.active()
-print("active:", conf["filters"]["testfilter"]["description"])
-assert conf["filters"]["testfilter"]["description"] == "nbr 1"
-print("Set conf2, 3, 4, 2, 3, 4, ...")
-for _ in range(100):
-    print(".", end="", flush=True)
-    cdsp.config.set_active_raw(configs[1])
-    time.sleep(0.001)
-    cdsp.config.set_active_raw(configs[2])
-    time.sleep(0.001)
-    cdsp.config.set_active_raw(configs[3])
-    time.sleep(0.5)
-    conf = cdsp.config.active()
-    desc = conf["filters"]["testfilter"]["description"]
-    assert conf["filters"]["testfilter"]["description"] == "nbr 4", f"{desc} != nbr 4"
+def test_100ms_via_ws(camillaclient):
+    change_with_delay(camillaclient ,0.1, 100)
+
+def test_1ms_via_ws(camillaclient):
+    change_with_delay(camillaclient, 0.001, 100)
