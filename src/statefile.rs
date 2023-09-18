de81cb782bec6920f4704b7995d1101b4ae2dff4
Henrik <henrik.enquist@gmail.com>
2023-09-18 19:30:49 +0200

    Stricter check that statefile write is successful

--- src/statefile.rs
+++ src/statefile.rs
@@ -26,7 +26,7 @@ pub fn load_state(filename: &str) -> Option<State> {
     let file = match File::open(filename) {
         Ok(f) => f,
         Err(err) => {
-            warn!("Could not open statefile '{filename}'. Error: {err}");
+            warn!("Could not read statefile '{filename}'. Error: {err}");
             return None;
         }
     };
@@ -35,14 +35,14 @@ pub fn load_state(filename: &str) -> Option<State> {
     let _number_of_bytes: usize = match buffered_reader.read_to_string(&mut contents) {
         Ok(number_of_bytes) => number_of_bytes,
         Err(err) => {
-            warn!("Could not open statefile '{filename}'. Error: {err}");
+            warn!("Could not read statefile '{filename}'. Error: {err}");
             return None;
         }
     };
     let state: State = match serde_yaml::from_str(&contents) {
         Ok(st) => st,
         Err(err) => {
-            warn!("Invalid statefile!\n{err}");
+            warn!("Invalid statefile, ignoring! Error:\n{err}");
             return None;
         }
     };
@@ -66,14 +66,21 @@ pub fn save_state(
         .open(filename)
     {
         Ok(f) => {
-            if let Err(writeerr) = serde_yaml::to_writer(f, &state) {
+            if let Err(writeerr) = serde_yaml::to_writer(&f, &state) {
                 error!(
-                    "Unable to write to statefile {}, error: {}",
+                    "Unable to write to statefile '{}', error: {}",
                     filename, writeerr
                 );
-            } else {
-                unsaved_changes.store(false, Ordering::Relaxed);
+                return;
             }
+            if let Err(syncerr) = &f.sync_all() {
+                error!(
+                    "Unable to commit statefile '{}' data to disk, error: {}",
+                    filename, syncerr
+                );
+                return;
+            }
+            unsaved_changes.store(false, Ordering::Relaxed);
         }
         Err(openerr) => error!("Unable to open statefile {}, error: {}", filename, openerr),
     }
