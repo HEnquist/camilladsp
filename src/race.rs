4ddb4052c98dac74cd386f7019a2202edb1970eb
Henrik <henrik.enquist@gmail.com>
2025-01-03 21:58:00 +0100

    WIP race processor

---
+++ src/race.rs
@@ -0,0 +1,175 @@
+// RACE, recursive audio crosstalk elimination (check!)
+
+use crate::audiodevice::AudioChunk;
+use crate::basicfilters::Delay;
+use crate::basicfilters::Gain;
+use crate::config;
+use crate::filters::Filter;
+use crate::filters::Processor;
+use crate::PrcFmt;
+use crate::Res;
+
+//#[derive(Debug)]
+pub struct RACE {
+    pub name: String,
+    pub channels: usize,
+    pub channel_a: usize,
+    pub channel_b: usize,
+    pub feedback_a: PrcFmt,
+    pub feedback_b: PrcFmt,
+    pub delay_a: Delay,
+    pub delay_b: Delay,
+    pub gain: Gain,
+    pub samplerate: usize,
+}
+
+impl RACE {
+    /// Creates a RACE processor from a config struct
+    pub fn from_config(name: &str, config: config::RACEParameters, samplerate: usize) -> Self {
+        let name = name.to_string();
+        let channels = config.channels;
+
+        //debug!("Creating compressor '{}', channels: {}, monitor_channels: {:?}, process_channels: {:?}, attack: {}, release: {}, threshold: {}, factor: {}, makeup_gain: {}, soft_clip: {}, clip_limit: {:?}",
+        //        name, channels, process_channels, monitor_channels, attack, release, config.threshold, config.factor, config.makeup_gain(), config.soft_clip(), clip_limit);
+
+        // TODO subtract one sample period from the delay, clamp at zero
+        let delayconf = config::DelayParameters {
+            delay: config.delay,
+            unit: config.delay_unit,
+            subsample: config.subsample_delay,
+        };
+        let delay_a = Delay::from_config("Delay A", samplerate, delayconf.clone());
+        let delay_b = Delay::from_config("Delay B", samplerate, delayconf);
+
+        let gainconfig = config::GainParameters {
+            gain: -config.attenuation,
+            scale: Some(config::GainScale::Decibel),
+            inverted: Some(true),
+            mute: Some(false),
+        };
+        let gain = Gain::from_config("Gain", gainconfig);
+
+        // sort channel numbers
+        let channel_a = config.channel_a.min(config.channel_b);
+        let channel_b = config.channel_a.max(config.channel_b);
+
+        RACE {
+            name,
+            channels,
+            samplerate,
+            channel_a,
+            channel_b,
+            delay_a,
+            delay_b,
+            gain,
+            feedback_a: 0.0,
+            feedback_b: 0.0,
+        }
+    }
+}
+
+impl Processor for RACE {
+    fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Apply a RACE processor to an AudioChunk, modifying it in-place.
+    fn process_chunk(&mut self, input: &mut AudioChunk) -> Res<()> {
+        let (first, second) = input.waveforms.split_at_mut(self.channel_b);
+        let channel_a = &mut first[self.channel_a];
+        let channel_b = &mut second[0];
+        if channel_a.is_empty() || channel_b.is_empty() {
+            return Ok(());
+        }
+        for (value_a, value_b) in channel_a.iter_mut().zip(channel_b.iter_mut()) {
+            // todo math
+            let added_a = *value_a + self.feedback_b;
+            let added_b = *value_b + self.feedback_a;
+            self.feedback_a = self.delay_a.process_single(added_a);
+            self.feedback_b = self.delay_b.process_single(added_b);
+            self.feedback_a = self.gain.process_single(self.feedback_a);
+            self.feedback_b = self.gain.process_single(self.feedback_b);
+            *value_a = added_a;
+            *value_b = added_b;
+        }
+        Ok(())
+    }
+
+    fn update_parameters(&mut self, config: config::Processor) {
+        if let config::Processor::RACE {
+            parameters: config, ..
+        } = config
+        {
+            self.channels = config.channels;
+
+            // TODO subtract one sample period from the delay, clamp at zero
+            let delayparams = config::DelayParameters {
+                delay: config.delay,
+                unit: config.delay_unit,
+                subsample: config.subsample_delay,
+            };
+            let delayconf = config::Filter::Delay {
+                parameters: delayparams,
+                description: None,
+            };
+            self.delay_a.update_parameters(delayconf.clone());
+            self.delay_b.update_parameters(delayconf);
+
+            let gainparams = config::GainParameters {
+                gain: -config.attenuation,
+                scale: Some(config::GainScale::Decibel),
+                inverted: Some(true),
+                mute: Some(false),
+            };
+            let gainconfig = config::Filter::Gain {
+                description: None,
+                parameters: gainparams,
+            };
+            self.gain.update_parameters(gainconfig);
+
+            // sort channel numbers
+            self.channel_a = config.channel_a.min(config.channel_b);
+            self.channel_b = config.channel_a.max(config.channel_b);
+
+            //debug!("Updated compressor '{}', monitor_channels: {:?}, process_channels: {:?}, attack: {}, release: {}, threshold: {}, factor: {}, makeup_gain: {}, soft_clip: {}, clip_limit: {:?}",
+            //    self.name, self.process_channels, self.monitor_channels, attack, release, config.threshold, config.factor, config.makeup_gain(), config.soft_clip(), clip_limit);
+        } else {
+            // This should never happen unless there is a bug somewhere else
+            panic!("Invalid config change!");
+        }
+    }
+}
+
+/// Validate the RACE processor config, to give a helpful message intead of a panic.
+pub fn validate_race(config: &config::RACEParameters) -> Res<()> {
+    let channels = config.channels;
+    if config.attenuation <= 0.0 {
+        let msg = "Attenuation value must be larger than zero.";
+        return Err(config::ConfigError::new(msg).into());
+    }
+    if config.delay <= 0.0 {
+        let msg = "Delay value must be larger than zero.";
+        return Err(config::ConfigError::new(msg).into());
+    }
+    if config.channel_a == config.channel_b {
+        let msg = "Channels a and b must be different";
+        return Err(config::ConfigError::new(msg).into());
+    }
+    if config.channel_a >= channels {
+        let msg = format!(
+            "Invalid channel a to process: {}, max is: {}.",
+            config.channel_a,
+            channels - 1
+        );
+        return Err(config::ConfigError::new(&msg).into());
+    }
+    if config.channel_b >= channels {
+        let msg = format!(
+            "Invalid channel b to process: {}, max is: {}.",
+            config.channel_b,
+            channels - 1
+        );
+        return Err(config::ConfigError::new(&msg).into());
+    }
+    Ok(())
+}
