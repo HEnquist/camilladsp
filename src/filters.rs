d5e6cff8ac02a6f4b9c8de3b9c0076f740e5015e
HEnquist <henrik.enquist@gmail.com>
2025-03-17 21:19:03 +0100

    Update to new audioadapter

--- src/filters.rs
+++ src/filters.rs
@@ -12,13 +12,12 @@ use crate::loudness;
 use crate::mixer;
 use crate::noisegate;
 use crate::race;
-use audioadapter::number_to_float::InterleavedNumbers;
+use audioadapter::readwrite::ReadSamples;
 use audioadapter::sample::{F32LE, F64LE, I16LE, I24LE, I32LE};
-use audioadapter::Adapter;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
-use std::io::{BufRead, Read, Seek, SeekFrom};
+use std::io::{BufRead, Seek, SeekFrom};
 use std::sync::Arc;
 use std::time::Instant;
 
@@ -117,43 +116,37 @@ pub fn read_coeff_file(
         // All other formats
         _ => {
             file.seek(SeekFrom::Start(skip_bytes_lines as u64))?;
-            let mut data = Vec::new();
-            let mut nbr_bytes = file.read_to_end(&mut data)?;
-            println!("Nbr bytes: {}", nbr_bytes);
-            if read_bytes_lines > 0 {
-                nbr_bytes = nbr_bytes.min(read_bytes_lines);
-            }
-            let nbr_coeffs = nbr_bytes / format.bytes_per_sample();
-            let adapter: Box<dyn Adapter<PrcFmt>> = match format {
-                config::FileFormat::S16LE => Box::new(
-                    InterleavedNumbers::<&[I16LE], PrcFmt>::new_from_bytes(&data, 1, nbr_coeffs)
-                        .unwrap(),
-                ),
-                config::FileFormat::S24LE3 => Box::new(
-                    InterleavedNumbers::<&[I24LE<3>], PrcFmt>::new_from_bytes(&data, 1, nbr_coeffs)
-                        .unwrap(),
-                ),
-                config::FileFormat::S24LE => Box::new(
-                    InterleavedNumbers::<&[I24LE<4>], PrcFmt>::new_from_bytes(&data, 1, nbr_coeffs)
-                        .unwrap(),
-                ),
-                config::FileFormat::S32LE => Box::new(
-                    InterleavedNumbers::<&[I32LE], PrcFmt>::new_from_bytes(&data, 1, nbr_coeffs)
-                        .unwrap(),
-                ),
-                config::FileFormat::FLOAT32LE => Box::new(
-                    InterleavedNumbers::<&[F32LE], PrcFmt>::new_from_bytes(&data, 1, nbr_coeffs)
-                        .unwrap(),
-                ),
-                config::FileFormat::FLOAT64LE => Box::new(
-                    InterleavedNumbers::<&[F64LE], PrcFmt>::new_from_bytes(&data, 1, nbr_coeffs)
-                        .unwrap(),
-                ),
+            let nbr_coeffs = read_bytes_lines / format.bytes_per_sample();
+
+            match *format {
+                config::FileFormat::S16LE => {
+                    file.read_converted_to_end::<I16LE, PrcFmt>(&mut coefficients)?;
+                }
+                config::FileFormat::S24LE3 => {
+                    file.read_converted_to_end::<I24LE<3>, PrcFmt>(&mut coefficients)?;
+                }
+                config::FileFormat::S24LE => {
+                    file.read_converted_to_end::<I24LE<4>, PrcFmt>(&mut coefficients)?;
+                }
+                config::FileFormat::S32LE => {
+                    file.read_converted_to_end::<I32LE, PrcFmt>(&mut coefficients)?;
+                }
+                config::FileFormat::FLOAT32LE => {
+                    file.read_converted_to_end::<F32LE, PrcFmt>(&mut coefficients)?;
+                }
+                config::FileFormat::FLOAT64LE => {
+                    file.read_converted_to_end::<F64LE, PrcFmt>(&mut coefficients)?;
+                }
                 config::FileFormat::TEXT => unreachable!(),
-            };
-            println!("Nbr coeffs: {}", nbr_coeffs);
-            coefficients.resize(nbr_coeffs, 0.0);
-            adapter.write_from_channel_to_slice(0, 0, &mut coefficients);
+            }
+            debug!("Read {} coeffs from file", coefficients.len());
+            if coefficients.len() > nbr_coeffs {
+                debug!(
+                    "Read more coefficients thatn needed, trimming to {}",
+                    nbr_coeffs
+                );
+                coefficients.resize(nbr_coeffs, 0.0);
+            }
         }
     }
     debug!(
